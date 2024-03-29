fe3d0211786ccae4eac41dd2050d507422f1ecb4
Nicole <nickii.cn@hotmail.com>
2024-03-29 11:47:53 -0300

    desafio entregable terminado

--- desafio.js
+++ desafio.js
@@ -8,7 +8,7 @@ class Product {
         this.code = code;
         this.stock = stock;
     }
-    
+
     generateId() {
         return '_' + Math.random().toString(36).substring(2, 9);
     }
@@ -43,11 +43,26 @@ class ProductManager {
 }
 
 const productManager = new ProductManager();
-productManager.getProducts()
 
-const getProductById = (productId) => {
-    const product = productManager.getProductById(productId);
-    return product;
+console.log('Productos después de la creación:', productManager.getProducts())
+
+productManager.addProduct(new Product("titulo", "Descripción", 20, "imagen1.jpg", "ABC123", 10));
+productManager.addProduct(new Product("titulo", "Descripción", 30, "imagen2.jpg", "DEF456", 15));
+
+console.log('Todos los productos:', productManager.getProducts())
+
+const newProduct = new Product("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+const isDuplicate = productManager.products.some(product => product.code === newProduct.code);
+if (isDuplicate) {
+    console.error('Error: El código del producto ya existe.');
+} else {
+    productManager.addProduct(newProduct);
 }
 
-getProductById(1)
+const productId = productManager.getProducts()[0].id;
+const product = productManager.products.find(product => product.id === productId);
+if (product) {
+    console.log('Producto obtenido por ID:', product);
+} else {
+    console.error('Error: Producto no encontrado.');
+}
